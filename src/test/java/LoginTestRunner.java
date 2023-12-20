35f3448bcf1c6b7223d49cd1666a46631976b276
salmansrabon <salmansrabon@gmail.com>
2023-12-20 21:02:49 +0600

    upload

---
+++ src/test/java/LoginTestRunner.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTestRunner extends Setup {;
+    LoginPage loginPage;
+    @Test(priority = 1)
+    public void doLoginWithWrongCreds(){
+        loginPage=new LoginPage(driver);
+        loginPage.doLogin("admin","wrongpass");
+        String textActual= driver.findElement(By.className("oxd-alert-content-text")).getText();
+        String textExpected="Invalid credentials";
+        Assert.assertTrue(textActual.contains(textExpected));
+    }
+    @Test(priority = 2)
+    public void doLoginWithValidCreds(){
+        loginPage=new LoginPage(driver);
+        loginPage.doLogin("admin","admin123");
+        Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"));
+        boolean isImageExists= driver.findElement(By.className("oxd-userdropdown-img")).isDisplayed();
+        Assert.assertTrue(isImageExists);
+    }
+    @Test (priority = 3)
+    public void doLogout(){
+        loginPage=new LoginPage(driver);
+        loginPage.doLogout();
+        String loginHeaderTitleActual= driver.findElement(By.className("orangehrm-login-title")).getText();
+        String loginHeaderExpected="Login";
+        Assert.assertEquals(loginHeaderTitleActual,loginHeaderExpected);
+    }
+}
---
+++ src/test/java/PIMPage.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class PIMPage {
+    @FindBy(className = "oxd-main-menu-item")
+    List<WebElement> menuItems;
+    @FindBy(className = "oxd-button")
+    List<WebElement> button;
+    @FindBy(name = "firstName")
+    WebElement txtFirstName;
+    @FindBy(name="lastName")
+    WebElement txtLastName;
+    @FindBy(className="oxd-switch-input")
+    WebElement toggleButton;
+    @FindBy(className = "oxd-input")
+    List<WebElement> txtFields;
+
+    public PIMPage(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+    public void createUser(String firstName, String lastName, String username, String password) throws InterruptedException {
+        menuItems.get(1).click(); //click PIM menu
+        Thread.sleep(2000);
+        button.get(2).click(); //click add button
+        Thread.sleep(2000);
+        txtFirstName.sendKeys(firstName);
+        txtLastName.sendKeys(lastName);
+        toggleButton.click();
+        txtFields.get(5).sendKeys(username); //insert username
+        txtFields.get(6).sendKeys(password); //insert password
+        txtFields.get(7).sendKeys(password); //insert confirm password
+        button.get(1).click(); //click submit button
+    }
+
+}
